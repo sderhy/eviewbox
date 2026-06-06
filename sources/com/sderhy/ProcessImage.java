366230bfb05b2c30b927306e7450ceff911106bc
sderhy <serge@derhy.com>
2026-06-06 11:01:44 +0200

    Normalize ProcessImage line endings

--- sources/com/sderhy/ProcessImage.java
+++ sources/com/sderhy/ProcessImage.java
@@ -7,7 +7,7 @@
 */
 
 package com.sderhy;
-import java.awt.image.* ; 
+import java.awt.image.* ;
 import java.awt.*;
 import java.awt.Image ;
 
@@ -28,7 +28,7 @@ public static Image brighten2( Component c,Image src, int dxLevel , int dyWindow
 	ImageFilter  filter = new Brighten2(brightness,contrast) ; // No,  think contrast/Brightness
 	ImageProducer producer = new FilteredImageSource(src.getSource(),filter);
 	trackImage(c, src = c.createImage(producer));
-	return src;	
+	return src;
 
 }
 
@@ -56,8 +56,8 @@ public static Image invertLut( Component c,Image src) {
 	ImageProducer producer = new FilteredImageSource(src.getSource(),filter);
 	trackImage(c, src = c.createImage(producer));
 	return src;
-	}	
-	
+	}
+
 public static Image invert( Component c,Image src) {
 	int w = 	src.getWidth(c) ;
 	int h =     src.getHeight(c);
@@ -66,42 +66,42 @@ public static Image invert( Component c,Image src) {
 	for(int i = 0; i< len ; i++)
 		pixels[i] = pixels[i] ^ 0xffffff ;
 	return c.createImage(new MemoryImageSource(w,h,pixels,0,w));
-	}	
+	}
 
 public static Image brighten( Component c,Image src) {
 	ImageFilter  filter = new Brighten() ;
 	ImageProducer producer = new FilteredImageSource(src.getSource(),filter);
 	trackImage(c, src = c.createImage(producer));
 	return src;
-	}	
+	}
 public static Image brighten( Component c,Image src, int br) {
 	ImageFilter  filter = new Brighten() ;
 	Brighten.setBrightness(br);
 	ImageProducer producer = new FilteredImageSource(src.getSource(),filter);
 	trackImage(c, src = c.createImage(producer));
 	return src;
-	}	
-	
+	}
 
-	
+
+
 public static Image grayIt( Component c,Image src) {
 	ImageFilter  filter = new GrayIt() ;
 	ImageProducer producer = new FilteredImageSource(src.getSource(),filter);
 	trackImage(c, src = c.createImage(producer));
 	return src;
-	}	
-	
-	
+	}
+
+
 public static Image pseudoColor( Component c,Image src) {
 	ImageFilter  filter = new PseudoColor() ;
 	ImageProducer producer = new FilteredImageSource(src.getSource(),filter);
 	trackImage(c, src = c.createImage(producer));
 	return src;
-	}	
+	}
 
 public static Image flipVertical(Component c, Image src ){
 	int w = 	src.getWidth(c) ;
-	int h =     src.getHeight(c);	
+	int h =     src.getHeight(c);
 	Image dest  = c.createImage( w , h ) ;
 	Graphics g = dest.getGraphics();
 	g.drawImage(src,w,h,0,0,0,0,w,h,c);
@@ -113,28 +113,28 @@ public static Image flipVertical(Component c, Image src ){
 
 public static Image flipHorizontal(Component c, Image src ){
 	int w = 	src.getWidth(c) ;
-	int h =     src.getHeight(c);	
+	int h =     src.getHeight(c);
 	Image dest  = c.createImage( w , h ) ;
 	Graphics g = dest.getGraphics();
 	g.drawImage(src,w,0,0,h,0,0,w,h,c);
 	g.dispose() ;
-	
+
 	trackImage(c, dest);
 	return dest ;
 }
 
 public static Image sharpen (Component c, Image src ){
 
-	int width   = 	src.getWidth(c) ; 
+	int width   = 	src.getWidth(c) ;
 	int height =     src.getHeight(c);
-	tools.Tools.gc() ;	
+	tools.Tools.gc() ;
 	int[] pixels = grabPixels(c,src);
-	
+
 		int[] pixels2;
 		int i, x, y, offset;
 		int rgb, rgb1, rgb2, rgb3, rgb4, rgb5, rgb6, rgb7, rgb8, newrgb;
 		int rsum = 0, gsum = 0, bsum = 0;
-	
+
 		pixels2 = new int[width * height];
 		for (i = 0; i < width * height; i++)
 			pixels2[i] = pixels[i];
@@ -175,25 +175,25 @@ public static Image sharpen (Component c, Image src ){
 					bsum = 255;
 				newrgb = ((rsum << 16) & 0xff0000) | ((gsum << 8 ) & 0xff00) | bsum;
 				pixels[offset] = (rgb & 0xff000000) | (newrgb & 0xffffff);
-				
+
 			}
 		}
-	
-	
-	return c.createImage(new MemoryImageSource(width,height,pixels,0,width));	
+
+
+	return c.createImage(new MemoryImageSource(width,height,pixels,0,width));
 	}//endOfSharpenImage
-	
+
 public static Image blur(Component c, Image src ){
 
-	int width = 	src.getWidth(c) ; 
+	int width = 	src.getWidth(c) ;
 	int height =     src.getHeight(c);
-	tools.Tools.gc() ;	
+	tools.Tools.gc() ;
 	int[] pixels = grabPixels(c,src);
 		int[] pixels2;
 		int i, x, y, offset;
 		int rgb, rgb1, rgb2, rgb3, rgb4, rgb5, rgb6, rgb7, rgb8, rgbmean;
 		int rsum = 0, gsum = 0, bsum = 0;
-	
+
 		pixels2 = new int[width * height];
 		for (i = 0; i < width * height; i++)
 			pixels2[i] = pixels[i];
@@ -222,17 +222,17 @@ public static Image blur(Component c, Image src ){
 
 		pixels2 = null ;
 		tools.Tools.gc() ;
-	
-		return c.createImage(new MemoryImageSource(width, height,pixels,0, width));	
-	}//endOfrotateImage() ;	
+
+		return c.createImage(new MemoryImageSource(width, height,pixels,0, width));
+	}//endOfrotateImage() ;
 
 
 public static Image rotateImage(Component c, Image src ){
 
-	int w = 	src.getWidth(c) ; 
+	int w = 	src.getWidth(c) ;
 	int i = 0;
 	int h =     src.getHeight(c);
-	tools.Tools.gc() ;	
+	tools.Tools.gc() ;
 	int[] pixels = grabPixels(c,src);
 	int[]  rotPixels = new int[w * h]  ;
 	for (int x = 0 ; x < w ; x++ )
@@ -240,16 +240,16 @@ public static Image rotateImage(Component c, Image src ){
 				rotPixels[i]  =  pixels[ (y * w) - x  +  w-1] ;
 				i++ ;
 				}
-	return c.createImage(new MemoryImageSource(h,w,rotPixels,0,h));	
-	}//endOfrotateImage() ;	
-	
-	
+	return c.createImage(new MemoryImageSource(h,w,rotPixels,0,h));
+	}//endOfrotateImage() ;
+
+
 public static Image rotateRight(Component c, Image src ){
 
-	int w = 	src.getWidth(c) ; 
+	int w = 	src.getWidth(c) ;
 	int i = 0;
 	int h =     src.getHeight(c);
-	tools.Tools.gc() ;	
+	tools.Tools.gc() ;
 	int[] pixels = grabPixels(c,src);
 	int[]  rotPixels = new int[w * h]  ;
 	for (int x = 0 ; x < w ; x++ )
@@ -257,8 +257,8 @@ public static Image rotateRight(Component c, Image src ){
 				rotPixels[i]  =  pixels[w * (h-y) + x - w] ;
 				i++ ;
 				}
-	return c.createImage(new MemoryImageSource(h,w,rotPixels,0,h));	
-	}//endOfrotateImage() ;	
+	return c.createImage(new MemoryImageSource(h,w,rotPixels,0,h));
+	}//endOfrotateImage() ;
 
 }//end Of Class
 
@@ -267,7 +267,7 @@ class Invert extends RGBImageFilter{
 	public Invert(){
 		canFilterIndexColorModel = true ;
 		}
-		
+
 	public int filterRGB(int x, int y, int rgb){
 		return (	(rgb & 0xff000000 )+		// alpha chanel
 				(rgb & 0xffffff) ^ 0xffffff); //xOr
@@ -284,32 +284,32 @@ class GrayIt extends RGBImageFilter{
 		int g = (rgb& 0x00ff00 ) >> 8 ;
 		int b = (rgb& 0x0000ff) ;
 		int gray = ((r*3) + (g*4) + (b*2))/9 ;
-		return  0xff000000|gray<<16|gray<<8|gray ;	
+		return  0xff000000|gray<<16|gray<<8|gray ;
 		}
-		
+
 }//endOfClassInvert
 
 class Brighten extends RGBImageFilter{
-	public  static int  percent  = 120 ; 
+	public  static int  percent  = 120 ;
 	public Brighten(){
 		canFilterIndexColorModel = true ;
 		percent =120;
 	}
-		
-	
+
+
 	public int filterRGB(int x, int y, int rgb){
-		
+
 		int r = (rgb& 0xff0000) >> 16 ;
 		int g = (rgb& 0x00ff00 ) >> 8 ;
 		int b = (rgb& 0x0000ff) ;
-		
+
 		r = Math.min(255, (r*percent)/100) ;
 		g = Math.min(255, (g*percent)/100) ;
 		b = Math.min(255, (b*percent)/100) ;
-		
-			return  0xff000000|r<<16|g<<8|b ;	
+
+			return  0xff000000|r<<16|g<<8|b ;
 		}
-	public static void setBrightness( int br){ 
+	public static void setBrightness( int br){
 		percent = br ;
 		if(percent <0 ) percent=0;
 	}
@@ -322,14 +322,14 @@ class Brighten extends RGBImageFilter{
 *	Second : it defines a brightness by multiplying the pixel value by
 *	a fixed number.
 *	if contrast value is 100 : nothing is changed.
-*	if brightness value is 100 : nothing is changed either .  
+*	if brightness value is 100 : nothing is changed either .
 */
 class Brighten2 extends RGBImageFilter{
 	int l , w;
-	
+
 	public  Brighten2(int brightness, int contrast){
 		canFilterIndexColorModel = true ;
-	  	if(brightness<0) brightness =0 ; // else inverse curve white/black
+		if(brightness<0) brightness =0 ; // else inverse curve white/black
 		l = brightness -100 ;
 		w = contrast ;
 		if (l > 255)  l = 255 ;
@@ -338,65 +338,65 @@ class Brighten2 extends RGBImageFilter{
 	}
 	// Set contrast first and brightness :
 	private final int filter(int color){
-	
+
 		color = (int) ( color * w / 100  ) +l;
 		if( color < 0) color = 0;
 		if( color > 255) color = 255 ;
 		return color ;
 	}
 	public int filterRGB(int x, int y, int rgb){
-	  
+
 		//int r = (rgb & 0xff0000) >> 16 ;
 		//int g = (rgb & 0x00ff00 ) >> 8 ;
 		int b = (rgb & 0x0000ff) ;
-		
+
 		//r = filter(r) ; g = filter(g) ;
 		 b = filter(b) ;
-		
+
 		/*
 		 r = Math.min(  	Math.max( (int)((i+j*r)*l/100),0)		,255);
 		 g = Math.min(  	Math.max( (int)((i+j*g)*l/100),0)		,255);
 		 b = Math.min(  	Math.max( (int)((i+j*b)*l/100),0)		,255);
 		*/
-		return  0xff000000|b<<16|b<<8|b ;	
+		return  0xff000000|b<<16|b<<8|b ;
 	  }
 }// En of Brighten3
 
 class PseudoColor extends RGBImageFilter{
 	java.util.Random rnd ;
-	int ra,ga,ba ;	
-	  
+	int ra,ga,ba ;
+
 	public PseudoColor(){
 		canFilterIndexColorModel = true ;
 		rnd=new java.util.Random();
 		ra = (int)( 255* rnd.nextFloat() );
 		ga = (int)( 255* rnd.nextFloat() );
-	 	ba = (int)( 255* rnd.nextFloat() );	
-		
-		
-		
-	 	}
-		
+		ba = (int)( 255* rnd.nextFloat() );
+
+
+
+		}
+
 	public int filterRGB(int x, int y, int rgb){
-		
+
 		//tools.Tools.debug(this , "appel de filterRGB") ;
 		int r = (rgb& 0xff0000) >> 16 ;
 		int g = (rgb& 0x00ff00 ) >> 8 ;
 		int b = (rgb& 0x0000ff) ;
-		
+
 		/*
 		int ra = (int)(r* rnd.nextFloat() );
 		int ga = (int)(g* rnd.nextFloat() );
-	 	int ba = (int)(b* rnd.nextFloat() );	
+		int ba = (int)(b* rnd.nextFloat() );
 	 */
 		r = Math.min(255, r^ra  ) ;
 		g = Math.min(255, g^ga	) ;
 		b = Math.min(255, b^ba) ;
-	 
-		
-			return  0xff000000|r<<16|g<<8|b ;	
+
+
+			return  0xff000000|r<<16|g<<8|b ;
 		}
-	
+
 }//endOfPseudoColor
 
 class RotateFilter extends ImageFilter
