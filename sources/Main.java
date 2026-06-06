59301c84b1b965ba431141b75adfe9e9b39c97e3
sderhy <serge@derhy.com>
2026-06-06 11:06:02 +0200

    Set macOS application menu name

--- sources/Main.java
+++ sources/Main.java
@@ -1,6 +1,9 @@
 public final  class Main
 {
+	private static final String APPLICATION_NAME = "Eviewbox";
+
 	public static void main(String args[]){
+		setApplicationName();
 		com.sderhy.MainClass MC = new com.sderhy.MainClass("EViewBox !");
 		if(args != null && args.length > 0){
 			try {
@@ -14,5 +17,11 @@ public final  class Main
 		com.sderhy.Winager.closeAll() ;
 		//System.exit(0);
 	}
+
+	private static void setApplicationName(){
+		System.setProperty("apple.awt.application.name", APPLICATION_NAME);
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", APPLICATION_NAME);
+	}
+
 	public String getAppletInfo(){ return " Eviewbox version beta";}
 }
